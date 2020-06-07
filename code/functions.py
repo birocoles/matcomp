e2704362c92b6976516c2cee2cdbac7021067782
birocoles <vandscoelho@gmail.com>
2020-06-07 13:04:18 -0300

    new tests for dot_real functions

--- code/functions.py
+++ code/functions.py
@@ -3,12 +3,108 @@ from numba import jit
 
 # dot product
 
-def dot_dumb(x, y):
+def dot_real_dumb(x, y, check_input=True):
+    '''
+    Compute the dot product of x and y, where
+    x, y are elements of R^N. The imaginary parts are ignored.
+
+    The code uses a simple "for" to iterate on the arrays.
+
+    Parameters
+    ----------
+    x, y : arrays 1D
+        Vectors with N elements.
+
+    check_input : boolean
+        If True, verify if the input is valid. Default is True.
+
+    Returns
+    -------
+    result : scalar
+        Dot product of x and y.
+    '''
+    if check_input is True:
+        assert x.ndim == y.ndim == 1, 'x and y must be 1D arrays'
+        assert x.size == y.size, 'x and y must have the same size'
+
+    result = 0
+    for i in range(x.size):
+        # the '.real' forces the code to use
+        # only the real part of the arrays
+        result += x.real[i]*y.real[i]
+
+    return result
+
+
+def dot_real_numpy(x, y, check_input=True):
+    '''
+    Compute the dot product of x and y, where
+    x, y are elements of R^N. The imaginary parts are ignored.
+
+    The code uses numpy.sum.
+
+    Parameters
+    ----------
+    x, y : arrays 1D
+        Vectors with N elements.
+
+    check_input : boolean
+        If True, verify if the input is valid. Default is True.
+
+    Returns
+    -------
+    result : scalar
+        Dot product of x and y.
+    '''
+    if check_input is True:
+        assert x.ndim == y.ndim == 1, 'x and y must be 1D arrays'
+        assert x.size == y.size, 'x and y must have the same size'
+
+    # the '.real' forces the code to use
+    # only the real part of the arrays
+    result = np.sum(x.real*y.real)
+
+    return result
+
+
+@jit(nopython=True)
+def dot_real_numba(x, y, check_input=True):
+    '''
+    Compute the dot product of x and y, where
+    x, y are elements of R^N. The imaginary parts are ignored.
+
+    The code uses numba.
+
+    Parameters
+    ----------
+    x, y : arrays 1D
+        Vectors with N elements.
+    check_input : boolean
+        If True, verify if the input is valid. Default is True.
+
+    Returns
+    -------
+    result : scalar
+        Dot product of x and y.
+    '''
+    if check_input is True:
+        assert x.ndim == y.ndim == 1, 'x and y must be 1D arrays'
+        assert x.size == y.size, 'x and y must have the same size'
+
+    result = 0
+    for i in range(x.size):
+        # the '.real' forces the code to use
+        # only the real part of the arrays
+        result += x.real[i]*y.real[i]
+
+    return result
+
+def dot_complex_dumb(x, y):
     '''
     Compute the dot product of x and y, where
     x, y are elements of C^N.
 
-    The code uses a simple for to iterate on the arrays.
+    The code uses a simple "for" to iterate on the arrays.
 
     Parameters
     ----------
@@ -23,14 +119,16 @@ def dot_dumb(x, y):
     assert x.ndim == y.ndim == 1, 'x and y must be 1D arrays'
     assert x.size == y.size, 'x and y must have the same size'
 
-    result = 0
-    for i in range(x.size):
-        result += x[i]*y[i]
+    result_real = dot_real_dumb(x.real, y.real, check_input=False)
+    result_real -= dot_real_dumb(x.imag, y.imag, check_input=False)
+    result_imag = dot_real_dumb(x.real, y.imag, check_input=False)
+    result_imag = dot_real_dumb(x.imag, y.real, check_input=False)
+    result = result_real + 1j*result_imag
 
     return result
 
 
-def dot_numpy(x, y):
+def dot_complex_numpy(x, y):
     '''
     Compute the dot product of x and y, where
     x, y are elements of C^N.
@@ -50,23 +148,27 @@ def dot_numpy(x, y):
     assert x.ndim == y.ndim == 1, 'x and y must be 1D arrays'
     assert x.size == y.size, 'x and y must have the same size'
 
-    result = np.sum(x*y)
+    result_real = dot_real_numpy(x.real, y.real, check_input=False)
+    result_real -= dot_real_numpy(x.imag, y.imag, check_input=False)
+    result_imag = dot_real_numpy(x.real, y.imag, check_input=False)
+    result_imag = dot_real_numpy(x.imag, y.real, check_input=False)
+    result = result_real + 1j*result_imag
 
     return result
 
 
 @jit(nopython=True)
-def dot_numba(x, y):
+def dot_complex_numba(x, y):
     '''
     Compute the dot product of x and y, where
-    x, y are elements of R^N.
+    x, y are elements of C^N.
 
     The code uses numba.
 
     Parameters
     ----------
     x, y : arrays 1D
-        Vectors with N real elements.
+        Vectors with N elements.
 
     Returns
     -------
@@ -76,81 +178,83 @@ def dot_numba(x, y):
     assert x.ndim == y.ndim == 1, 'x and y must be 1D arrays'
     assert x.size == y.size, 'x and y must have the same size'
 
-    result = 0
-    for i in range(x.size):
-        result += x[i]*y[i]
+    result_real = dot_real_numba(x.real, y.real, check_input=False)
+    result_real -= dot_real_numba(x.imag, y.imag, check_input=False)
+    result_imag = dot_real_numba(x.real, y.imag, check_input=False)
+    result_imag = dot_real_numba(x.imag, y.real, check_input=False)
+    result = result_real + 1j*result_imag
 
     return result
 
 
 # Fourier Transform
 
-def DFT_dumb(x, scale=None):
-    '''
-    Compute the Discrete Fourier Transform (DFT) of the 1D array x.
-
-    The code is a slow version of the algorithm.
-
-    Parameters
-    ----------
-    x : array 1D
-        Vector with N elements.
-    scale : {None, 'ortho'}, optional
-        None (the default normalization) has the transform unscaled
-        and requires the inverse transform to be scaled by 1/N.
-        'sqrtn' scales the transform by 1/sqrt{N} and requires the inverse
-        transform to be scaled by 1/sqrt{N}. In this case, the FT is unitary.
-
-    Returns
-    -------
-    X : array 1D
-        DFT of x.
-    '''
-    assert scale in [None, 'sqrtn'], "scale must be None or 'sqrtn'"
-    x = np.asarray(x)
-    # define the Fourier matrix
-    N = x.size
-    kn = np.outer(np.arange(N), np.arange(N))
-    Fn = np.exp(-2j * np.pi * kn / N)
-    # compute the DFT
-    if scale is None:
-        X = np.dot(Fn, x)
-    else:
-        X = np.dot(Fn, x)/np.sqrt(N)
-
-    return X
-
-def IDFT_dumb(x, scale='N'):
-    '''
-    Compute the Inverse Discrete Fourier Transform (IDFT) of the 1D array x.
-
-    The code is a slow version of the algorithm.
-
-    Parameters
-    ----------
-    x : array 1D
-        Vector with N elements.
-    scale : {None, 'ortho'}, optional
-        'N' (the default normalization) has the transform unscaled
-        and requires the inverse transform to be scaled by 1/N.
-        'sqrtn' scales the transform by 1/sqrt{N} and requires the inverse
-        transform to be scaled by 1/sqrt{N}. In this case, the FT is unitary.
-
-    Returns
-    -------
-    X : array 1D
-        DFT of x.
-    '''
-    assert scale in ['N', 'sqrtn'], "scale must be 'N' or 'sqrtn'"
-    x = np.asarray(x)
-    # define the Fourier matrix
-    N = x.size
-    kn = np.outer(np.arange(N), np.arange(N))
-    Fn = np.exp(2j * np.pi * kn / N)
-    # compute the DFT
-    if scale is 'N':
-        X = np.dot(Fn, x)/N
-    else:
-        X = np.dot(Fn, x)/np.sqrt(N)
-
-    return X
+# def DFT_dumb(x, scale=None):
+#     '''
+#     Compute the Discrete Fourier Transform (DFT) of the 1D array x.
+#
+#     The code is a slow version of the algorithm.
+#
+#     Parameters
+#     ----------
+#     x : array 1D
+#         Vector with N elements.
+#     scale : {None, 'ortho'}, optional
+#         None (the default normalization) has the transform unscaled
+#         and requires the inverse transform to be scaled by 1/N.
+#         'sqrtn' scales the transform by 1/sqrt{N} and requires the inverse
+#         transform to be scaled by 1/sqrt{N}. In this case, the FT is unitary.
+#
+#     Returns
+#     -------
+#     X : array 1D
+#         DFT of x.
+#     '''
+#     assert scale in [None, 'sqrtn'], "scale must be None or 'sqrtn'"
+#     x = np.asarray(x)
+#     # define the Fourier matrix
+#     N = x.size
+#     kn = np.outer(np.arange(N), np.arange(N))
+#     Fn = np.exp(-2j * np.pi * kn / N)
+#     # compute the DFT
+#     if scale is None:
+#         X = np.dot(Fn, x)
+#     else:
+#         X = np.dot(Fn, x)/np.sqrt(N)
+#
+#     return X
+#
+# def IDFT_dumb(x, scale='N'):
+#     '''
+#     Compute the Inverse Discrete Fourier Transform (IDFT) of the 1D array x.
+#
+#     The code is a slow version of the algorithm.
+#
+#     Parameters
+#     ----------
+#     x : array 1D
+#         Vector with N elements.
+#     scale : {None, 'ortho'}, optional
+#         'N' (the default normalization) has the transform unscaled
+#         and requires the inverse transform to be scaled by 1/N.
+#         'sqrtn' scales the transform by 1/sqrt{N} and requires the inverse
+#         transform to be scaled by 1/sqrt{N}. In this case, the FT is unitary.
+#
+#     Returns
+#     -------
+#     X : array 1D
+#         DFT of x.
+#     '''
+#     assert scale in ['N', 'sqrtn'], "scale must be 'N' or 'sqrtn'"
+#     x = np.asarray(x)
+#     # define the Fourier matrix
+#     N = x.size
+#     kn = np.outer(np.arange(N), np.arange(N))
+#     Fn = np.exp(2j * np.pi * kn / N)
+#     # compute the DFT
+#     if scale is 'N':
+#         X = np.dot(Fn, x)/N
+#     else:
+#         X = np.dot(Fn, x)/np.sqrt(N)
+#
+#     return X
--- code/test_functions.py
+++ code/test_functions.py
@@ -5,113 +5,167 @@ import functions as fcs
 
 ### dot_product
 
-def test_dot_not_1D_arrays():
-    'check input not 1D arrays'
+def test_dot_real_not_1D_arrays():
+    'fail due to input that is not 1D array'
     vector_1 = np.ones((3,2))
     vector_2 = np.arange(4)
     with pytest.raises(AssertionError):
-        fcs.dot_dumb(vector_1, vector_2)
-        fcs.dot_numpy(vector_1, vector_2)
-        fcs.dot_numba(vector_1, vector_2)
+        fcs.dot_real_dumb(vector_1, vector_2)
+        fcs.dot_real_numpy(vector_1, vector_2)
+        fcs.dot_real_numba(vector_1, vector_2)
 
 
-def test_dot_different_sizes():
-    'check input with different sizes'
+def test_dot_real_different_sizes():
+    'fail due to inputs having different sizes'
     vector_1 = np.linspace(5,6,7)
     vector_2 = np.arange(4)
     with pytest.raises(AssertionError):
-        fcs.dot_dumb(vector_1, vector_2)
-        fcs.dot_numpy(vector_1, vector_2)
-        fcs.dot_numba(vector_1, vector_2)
+        fcs.dot_real_dumb(vector_1, vector_2)
+        fcs.dot_real_numpy(vector_1, vector_2)
+        fcs.dot_real_numba(vector_1, vector_2)
 
 
-def test_dot_known_values():
+def test_dot_real_known_values():
     'check output produced by specific input'
     vector_1 = 0.1*np.ones(10)
     vector_2 = np.linspace(23.1, 52, 10)
     reference_output = np.mean(vector_2)
-    computed_output_dumb = fcs.dot_dumb(vector_1, vector_2)
-    computed_output_numpy = fcs.dot_numpy(vector_1, vector_2)
-    computed_output_numba = fcs.dot_numba(vector_1, vector_2)
+    computed_output_dumb = fcs.dot_real_dumb(vector_1, vector_2)
+    computed_output_numpy = fcs.dot_real_numpy(vector_1, vector_2)
+    computed_output_numba = fcs.dot_real_numba(vector_1, vector_2)
     aae(reference_output, computed_output_dumb, decimal=10)
     aae(reference_output, computed_output_numpy, decimal=10)
     aae(reference_output, computed_output_numba, decimal=10)
 
 
-def test_dot_compare_numpy_dot():
+def test_dot_real_compare_numpy_dot():
     'compare with numpy.dot'
     np.random.seed = 41
     vector_1 = np.random.rand(13)
     vector_2 = np.random.rand(13)
     reference_output_numpy = np.dot(vector_1, vector_2)
-    computed_output_dumb = fcs.dot_dumb(vector_1, vector_2)
-    computed_output_numpy = fcs.dot_numpy(vector_1, vector_2)
-    computed_output_numba = fcs.dot_numba(vector_1, vector_2)
+    computed_output_dumb = fcs.dot_real_dumb(vector_1, vector_2)
+    computed_output_numpy = fcs.dot_real_numpy(vector_1, vector_2)
+    computed_output_numba = fcs.dot_real_numba(vector_1, vector_2)
     aae(reference_output_numpy, computed_output_dumb, decimal=10)
     aae(reference_output_numpy, computed_output_numpy, decimal=10)
     aae(reference_output_numpy, computed_output_numba, decimal=10)
 
 
-def test_dot_verify_complex_input():
-    'verify results produced with complex inputs'
+def test_dot_real_commutativity():
+    'verify commutativity'
+    np.random.seed = 19
+    a = np.random.rand(15)
+    b = np.random.rand(15)
+    # a dot b = b dot a
+    output_ab_dumb = fcs.dot_real_dumb(a, b)
+    output_ba_dumb = fcs.dot_real_dumb(b, a)
+    output_ab_numpy = fcs.dot_real_numpy(a, b)
+    output_ba_numpy = fcs.dot_real_numpy(b, a)
+    output_ab_numba = fcs.dot_real_numba(a, b)
+    output_ba_numba = fcs.dot_real_numba(b, a)
+    aae(output_ab_dumb, output_ba_dumb, decimal=10)
+    aae(output_ab_numpy, output_ba_numpy, decimal=10)
+    aae(output_ab_numba, output_ba_numba, decimal=10)
+
+
+def test_dot_real_distributivity():
+    'verify distributivity over sum'
+    np.random.seed = 19
+    a = np.random.rand(15)
+    b = np.random.rand(15)
+    c = np.random.rand(15)
+    # a dot (b + c) = (a dot b) + (a dot c)
+    output_a_bc_dumb = fcs.dot_real_dumb(a, b + c)
+    output_ab_ac_dumb = fcs.dot_real_dumb(a, b) + fcs.dot_real_dumb(a, c)
+    output_a_bc_numpy = fcs.dot_real_dumb(a, b + c)
+    output_ab_ac_numpy = fcs.dot_real_dumb(a, b) + fcs.dot_real_dumb(a, c)
+    output_a_bc_numba = fcs.dot_real_dumb(a, b + c)
+    output_ab_ac_numba = fcs.dot_real_dumb(a, b) + fcs.dot_real_dumb(a, c)
+    aae(output_a_bc_dumb, output_ab_ac_dumb, decimal=10)
+    aae(output_a_bc_numpy, output_ab_ac_numpy, decimal=10)
+    aae(output_a_bc_numba, output_ab_ac_numba, decimal=10)
+
+
+def test_dot_real_scalar_multiplication():
+    'verify scalar multiplication property'
+    np.random.seed = 8
+    a = np.random.rand(15)
+    b = np.random.rand(15)
+    c1 = 5.6
+    c2 = 9.1
+    # (c1 a) dot (c2 b) = c1c2 (a dot b)
+    output_c1a_c2b_dumb = fcs.dot_real_dumb(c1*a, c2*b)
+    output_c1c2_ab_dumb = c1*c2*fcs.dot_real_dumb(a, b)
+    output_c1a_c2b_numpy = fcs.dot_real_numpy(c1*a, c2*b)
+    output_c1c2_ab_numpy = c1*c2*fcs.dot_real_numpy(a, b)
+    output_c1a_c2b_numba = fcs.dot_real_numba(c1*a, c2*b)
+    output_c1c2_ab_numba = c1*c2*fcs.dot_real_numba(a, b)
+    aae(output_c1a_c2b_dumb, output_c1c2_ab_dumb, decimal=10)
+    aae(output_c1a_c2b_numpy, output_c1c2_ab_numpy, decimal=10)
+    aae(output_c1a_c2b_numba, output_c1c2_ab_numba, decimal=10)
+
+
+def test_dot_complex_compare_numpy_dot():
+    'compare with numpy.dot'
     # first input complex
     np.random.seed = 3
     vector_1 = np.random.rand(13) + np.random.rand(13)*1j
     vector_2 = np.random.rand(13)
-    computed_output = fcs.dot_numpy(vector_1, vector_2)
-    expected_result_real = np.sum(vector_1.real*vector_2.real)
-    expected_result_real -= np.sum(vector_1.imag*vector_2.imag)
-    expected_result_imag = np.sum(vector_1.real*vector_2.imag)
-    expected_result_imag += np.sum(vector_1.imag*vector_2.real)
-    expected_result = expected_result_real + expected_result_imag*1j
-    aae(computed_output, expected_result, decimal=10)
+    computed_output_dumb = fcs.dot_complex_dumb(vector_1, vector_2)
+    computed_output_numpy = fcs.dot_complex_numpy(vector_1, vector_2)
+    computed_output_numba = fcs.dot_complex_numba(vector_1, vector_2)
+    computed_output_numpy = np.dot(vector_1, vector_2)
+    aae(computed_output_dumb, computed_output_numpy, decimal=10)
+    aae(computed_output_numpy, computed_output_numpy, decimal=10)
+    aae(computed_output_numba, computed_output_numpy, decimal=10)
 
 
 # Discrete Fourier Transform (DFT) and Inverse Discrete Fourier Transform (IDFT)
 
-def test_DFT_invalid_scalar():
-    'check error for invalid scaling factor'
-    data = np.zeros(10)
-    invalid_scales = [data.size, np.sqrt(data.size), 'n', 'srqtn']
-    with pytest.raises(AssertionError):
-        for invalid_scale in invalid_scales:
-            fcs.DFT_dumb(x=data, scale=invalid_scale)
-
-
-def test_IDFT_invalid_scalar():
-    'check error for invalid scaling factor'
-    data = np.zeros(10)
-    invalid_scales = [data.size, np.sqrt(data.size), 'n', 'srqtn']
-    with pytest.raises(AssertionError):
-        for invalid_scale in invalid_scales:
-            fcs.IDFT_dumb(x=data, scale=invalid_scale)
-
-
-def test_DFT_compare_numpy_fft_fft():
-    'compare with numpy.fft.fft'
-    np.random.seed = 56
-    # unscaled DFT
-    data = np.random.rand(100)
-    reference_output_numpy = np.fft.fft(a=data, norm=None)
-    computed_output_dumb = fcs.DFT_dumb(x=data, scale=None)
-    aae(reference_output_numpy, computed_output_dumb, decimal=10)
-    # scaled DFT
-    data = np.random.rand(100)
-    reference_output_numpy = np.fft.fft(a=data, norm='ortho')
-    computed_output_dumb = fcs.DFT_dumb(x=data, scale='sqrtn')
-    aae(reference_output_numpy, computed_output_dumb, decimal=10)
-
-
-def test_IDFT_compare_numpy_fft_ifft():
-    'compare with numpy.fft.ifft'
-    np.random.seed = 4
-    # unscaled DFT
-    data = np.random.rand(100)+1j*np.random.rand(100)
-    reference_output_numpy = np.fft.ifft(a=data, norm=None)
-    computed_output_dumb = fcs.IDFT_dumb(x=data, scale='N')
-    aae(reference_output_numpy, computed_output_dumb, decimal=10)
-    # scaled DFT
-    data = np.random.rand(100)+1j*np.random.rand(100)
-    reference_output_numpy = np.fft.ifft(a=data, norm='ortho')
-    computed_output_dumb = fcs.IDFT_dumb(x=data, scale='sqrtn')
-    aae(reference_output_numpy, computed_output_dumb, decimal=10)
+# def test_DFT_invalid_scalar():
+#     'check error for invalid scaling factor'
+#     data = np.zeros(10)
+#     invalid_scales = [data.size, np.sqrt(data.size), 'n', 'srqtn']
+#     with pytest.raises(AssertionError):
+#         for invalid_scale in invalid_scales:
+#             fcs.DFT_dumb(x=data, scale=invalid_scale)
+#
+#
+# def test_IDFT_invalid_scalar():
+#     'check error for invalid scaling factor'
+#     data = np.zeros(10)
+#     invalid_scales = [data.size, np.sqrt(data.size), 'n', 'srqtn']
+#     with pytest.raises(AssertionError):
+#         for invalid_scale in invalid_scales:
+#             fcs.IDFT_dumb(x=data, scale=invalid_scale)
+#
+#
+# def test_DFT_compare_numpy_fft_fft():
+#     'compare with numpy.fft.fft'
+#     np.random.seed = 56
+#     # unscaled DFT
+#     data = np.random.rand(100)
+#     reference_output_numpy = np.fft.fft(a=data, norm=None)
+#     computed_output_dumb = fcs.DFT_dumb(x=data, scale=None)
+#     aae(reference_output_numpy, computed_output_dumb, decimal=10)
+#     # scaled DFT
+#     data = np.random.rand(100)
+#     reference_output_numpy = np.fft.fft(a=data, norm='ortho')
+#     computed_output_dumb = fcs.DFT_dumb(x=data, scale='sqrtn')
+#     aae(reference_output_numpy, computed_output_dumb, decimal=10)
+#
+#
+# def test_IDFT_compare_numpy_fft_ifft():
+#     'compare with numpy.fft.ifft'
+#     np.random.seed = 4
+#     # unscaled DFT
+#     data = np.random.rand(100)+1j*np.random.rand(100)
+#     reference_output_numpy = np.fft.ifft(a=data, norm=None)
+#     computed_output_dumb = fcs.IDFT_dumb(x=data, scale='N')
+#     aae(reference_output_numpy, computed_output_dumb, decimal=10)
+#     # scaled DFT
+#     data = np.random.rand(100)+1j*np.random.rand(100)
+#     reference_output_numpy = np.fft.ifft(a=data, norm='ortho')
+#     computed_output_dumb = fcs.IDFT_dumb(x=data, scale='sqrtn')
+#     aae(reference_output_numpy, computed_output_dumb, decimal=10)
